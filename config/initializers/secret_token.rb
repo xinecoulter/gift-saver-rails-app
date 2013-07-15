77bc321ed1fc47a2500d96ac1f29e49bfd67428d
Christine Coulter <christinetcoulter@gmail.com>
2013-07-15 17:13:27 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftsaverzzz::Application.config.secret_token = '39c208486bf7e6edac317ff237d105ae4ff514ab5a68fb264ecec016bb52561dab6cd931fc94b8ef09862de645a4fef184525f51af7f69e4eaf2a0924145a639'
