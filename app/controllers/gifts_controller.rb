d103704ac09d6c90746d81569b473bb369e8d0c4
Christine Coulter <christinetcoulter@gmail.com>
2013-07-17 00:20:53 -0400

    Edit new action in gifts_controller to include aws search for books case. Edit new.html.erb file within gifts views to list results of authors.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -13,9 +13,20 @@ class GiftsController < ApplicationController
   def new
     @user = User.where(username: params[:username]).first
     if params[:name]
-      @results = params[:name]
+      item_search = ItemSearch.new(params[:category], { 'Title' => params[:name] })
+      response_group = ResponseGroup.new( 'Medium' )
+      request = Request.new
+      results = request.search(item_search, response_group)
+      @results = results.item_search_response[0].items[0].item[0..9]
     else
       @results = nil
     end
   end
 end
+
+# is = ItemSearch.new( 'Books', { 'Title' => 'Ruby' } )
+# rg = ResponseGroup.new( 'Small' )
+# req = Request.new
+# resp = req.search( is, rg )
+# items = resp.item_search_response[0].items[0].item THIS IS AN ARRAY OF ITEMS RETURNED
+# resp.item_search_response[0].items[0].item[0].asin
